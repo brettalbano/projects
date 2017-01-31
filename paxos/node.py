1814d778e2c9d2a6feb43b4711ebb6dbc9494e55
brettalbano <brettalbano@gmail.com>
2017-01-31 14:22:46 -0800

    Add class descriptions

--- paxos/node.py
+++ paxos/node.py
@@ -1,2 +1,14 @@
-#!/usr/bin/python
+'''
+Provides the Node class
+'''
+
+import port
+import ethernet
+
+class Node():
+  '''
+  This class will be the paxos members. It will listen to elections and
+  proposals of other nodes and send out information through the port
+  and ethernet classes.
+  '''
 
